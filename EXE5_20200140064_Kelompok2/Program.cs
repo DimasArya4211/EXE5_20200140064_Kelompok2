12150947ee14706ef746cf5d1dc2769e948e87d6
ACER <ACER@LAPTOP-PJA37DT8>
2022-12-21 15:40:08 +0700

    Add project files.

---
+++ EXE5_20200140064_Kelompok2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_20200140064_Kelompok2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
